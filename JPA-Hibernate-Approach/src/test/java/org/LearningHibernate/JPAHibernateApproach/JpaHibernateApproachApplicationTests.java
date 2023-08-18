cd6c3ef0c8759eca370426c7f6ee1c2f2b836841
Raghav9755 <raghav.agarwal975555@gmail.com>
2023-08-18 14:30:24 +0530

    Add jpa Hibernate folder

---
+++ JPA-Hibernate-Approach/src/test/java/org/LearningHibernate/JPAHibernateApproach/JpaHibernateApproachApplicationTests.java
@@ -0,0 +1,13 @@
+package org.LearningHibernate.JPAHibernateApproach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApproachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
